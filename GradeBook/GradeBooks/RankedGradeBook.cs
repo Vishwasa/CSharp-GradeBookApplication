b49c4ab8869e24f3d01e76a0bb0e062c781d336e
Vishwasa <mail2vishwas.sl@gmail.com>
2018-08-09 18:19:56 +0530

    changes

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,11 +6,20 @@ using System.Collections.Generic;
 namespace GradeBook.GradeBooks
 
 {
-    public class RankedGradeBook:BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name):base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+                base.CalculateStatistics();
         }
         public override char GetLetterGrade(double averageGrade)
         {
@@ -22,14 +31,22 @@ namespace GradeBook.GradeBooks
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
             if (grades[threshold - 1] <= averageGrade)
                 return 'A';
-            else if (grades[threshold*2 - 1] <= averageGrade)
+            else if (grades[threshold * 2 - 1] <= averageGrade)
                 return 'B';
-            else if (grades[threshold*3 - 1] <= averageGrade)
+            else if (grades[threshold * 3 - 1] <= averageGrade)
                 return 'C';
-            else if (grades[threshold*4 - 1] <= averageGrade)
+            else if (grades[threshold * 4 - 1] <= averageGrade)
                 return 'D';
             else
                 return 'F';
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
