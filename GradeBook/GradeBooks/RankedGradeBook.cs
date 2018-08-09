cc7bd2c78fffc3e02691b3245512696a5370d5c4
Vishwasa <mail2vishwas.sl@gmail.com>
2018-08-09 17:15:06 +0530

    RankedGradeBook changes for chapter 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,7 +1,10 @@
 ﻿using GradeBook.Enums;
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace GradeBook.GradeBooks
+
 {
     public class RankedGradeBook:BaseGradeBook
     {
@@ -9,14 +12,25 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
-        public override char GetLetterGrade(double x)
+        public override char GetLetterGrade(double averagegrade)
         {
             if (Students.Count<5)
             {
                 InvalidOperationException invalidOperationException = new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
                 throw invalidOperationException;
             }
-            return base.GetLetterGrade(x);
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            if (grades[threshold - 1] <= averagegrade)
+                return 'A';
+            else if (grades[threshold*2 - 1] <= averagegrade)
+                return 'B';
+            else if (grades[threshold*3 - 1] <= averagegrade)
+                return 'C';
+            else if (grades[threshold*4 - 1] <= averagegrade)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
